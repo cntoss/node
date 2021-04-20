f98682c6fca4638ede01849e0e4e877eafc348bb
ad01santosh <ad01santosh@gmail.com>
2021-04-20 16:45:50 +0545

    express routing

--- node_express/index.js
+++ node_express/index.js
@@ -1,8 +1,11 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const dishRouter = require('./router/dishRouter');
 http = require('http');
 morgan = require('morgan');
 bodyParse = require('body-parser');
+
+const dishROuter = require('./router/dishRouter');
 
 const hostname = 'localhost';
 const port = '3000';
@@ -12,52 +15,7 @@ app.use(morgan('dev'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
-app.all('/dishes', (req, res, next) => {
-    res.statusCode = 200;
-    res.setHeader('Content-type', 'text/plain');
-    next();
-});
-
-
-app.get('/dishes', (req, res, next) => {
-    res.end('Will send all dishes information to you');
-});
-
-app.post('/dishes', (req, res, next) => {
-    res.end('Will send dishe: ' + req.body.name + 
-    ' with details ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
-    res.statusCode = 401;
-    res.end('Put operation did not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
-    res.end('Deleting all dishes information');
-});
-
-//for single id
-app.get('/dishes/:dishId', (req, res, next) => {
-    res.end('Will send details of dish ' +
-    req.params.dishId + ' to you');
-});
-
-app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 401;
-    res.end('Post operation did not supported on /dishes/' +
-    req.params.dishId);
-});
-
-app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updationg the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-    ' with details: ' + req.body.description);
-});
-
-app.delete('/dishes/:dishId', (req, res, next) => {
-    res.end('Deleting dishe ' + req.params.dishId);
-});
+app.use('/dishes', dishRouter);
 
 app.use((req, res, next) => {
     res.statusCode = 200;
--- node_express/router/dishRouter.js
+++ node_express/router/dishRouter.js
@@ -3,29 +3,27 @@ const bodyParser = require('body-parser');
 
 const dishRouter = express.Router();
 
+dishRouter.use(bodyParser.json());
+
 dishRouter.route('/')
 .all((req, res, next) => {
     res.statusCode = 200;
     res.setHeader('Content-type', 'text/plain');
     next();
-});
-
-
-app.get('/dishes', (req, res, next) => {
+})
+.get((req, res, next) => {
     res.end('Will send all dishes information to you');
-});
-
-app.post('/dishes', (req, res, next) => {
+})
+.post((req, res, next) => {
     res.end('Will send dishe: ' + req.body.name + 
     ' with details ' + req.body.description);
-});
-
-app.put('/dishes', (req, res, next) => {
+})
+.put((req, res, next) => {
     res.statusCode = 401;
     res.end('Put operation did not supported on /dishes');
-});
-
-app.delete('/dishes', (req, res, next) => {
+})
+.delete((req, res, next) => {
     res.end('Deleting all dishes information');
 });
 
+module.exports = dishRouter;
